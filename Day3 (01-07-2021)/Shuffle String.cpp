f1058b0985fd7bb95ebac5d22a9b5addb1b9ef0a
sid <sidd.bidaliya2000@gmail.com>
2021-07-05 21:21:33 +0530

    Remaining Questions

---
+++ Day3 (01-07-2021)/Shuffle String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        map<int,char> m;
+        for(int i=0;i<indices.size();i++)
+            m[indices[i]]=s[i];
+        
+        string s1;
+         for(int i=0;i<indices.size();i++)
+             s1.push_back(m[i]);
+
+        return s1;
+    }
+};
